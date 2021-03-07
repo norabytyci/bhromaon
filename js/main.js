04ff65a1fe7988b9c9f36ae1ee50a0d3121d88c8
Nora Bytyci <nora.bytyci@student.uni-pr.edu>
2021-03-07 14:19:07 +0100

    Exam submitted

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".header__nav ul");
 
-hamburger.addEventListener("click", function(item) {
+hamburger.addEventListener("click", function() {
     this.classList.toggle("open");
     nav.classList.toggle("open");
 })
